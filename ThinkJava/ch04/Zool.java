c9165fa7ec8812ee55ffa5b88c6f95016f7600c3
JhovanyBadillo <57308567+JhovanyBadillo@users.noreply.github.com>
2019-11-03 12:32:03 -0600

    Add files via upload

---
+++ ThinkJava/ch04/Zool.java
@@ -0,0 +1,8 @@
+public class Zool{
+	public static void zool(int i, String s1, String s2){
+		System.out.println(i + " " + s1 + " " + s2);
+	}
+	public static void main(String[] args){
+		zool(1,"hola","Ana");
+	}
+}
