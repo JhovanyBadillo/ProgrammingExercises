b85b9a833502799d10e2bc191b527547f203df90
JhovanyBadillo <57308567+JhovanyBadillo@users.noreply.github.com>
2019-11-03 12:04:25 -0600

    Create Date.java

---
+++ ThinkJava/ch01/Date.java
@@ -0,0 +1,19 @@
+public class Date{
+	public static void main(String[] args){
+		System.out.println("Today is Sunday");
+		String day = "Sunday";
+		int date = 27;
+		String month = "October";
+		int year = 2019;
+		System.out.println("American format:");
+		System.out.print(day);
+		System.out.print(", ");
+		System.out.print(month + " ");
+		System.out.print(date);
+		System.out.print(", ");
+		System.out.println(year);
+
+		System.out.println("European format:");
+		System.out.println(day + " " + date + " " + month + " " + year);
+	}
+}
